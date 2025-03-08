b5d5d81f2ab7e8afaafedfe6770a6a636c49fd87
Shyamalendu Nayak <shyamalendu.nayak@hyscaler.com>
2025-03-08 22:31:49 +0530

    Speech Recognition added

---
+++ components/SpeechRecognition/SpeechRecognition.tsx
@@ -0,0 +1,33 @@
+import { Mic } from "lucide-react";
+import React, { useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+interface SpeechRecognitionProps {
+  setSourceText: any;
+}
+
+const SpeechRecognitionComponent = ({
+  setSourceText,
+}: SpeechRecognitionProps) => {
+  const { transcript, listening } = useSpeechRecognition();
+  useEffect(() => {
+    setSourceText(transcript);
+  }, [transcript, setSourceText]);
+
+  const handleVoiceRecording = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening();
+    }
+  };
+  return (
+    <div>
+      <Mic size={24} className="text-gray-400" onClick={handleVoiceRecording} />
+    </div>
+  );
+};
+
+export default SpeechRecognitionComponent;
