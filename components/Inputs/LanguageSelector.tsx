880e7388c31629b1e0b1966d7d53a08ffc6768dd
Shyamalendu Nayak <shyamalendunayak0118@gmail.com>
2025-03-12 07:41:50 +0530

    Fixed the type error for language select prop

--- components/Inputs/LanguageSelector.tsx
+++ components/Inputs/LanguageSelector.tsx
@@ -2,12 +2,12 @@ import { Languages } from "lucide-react";
 import React from "react";
 
 interface LanguageSelectorProps {
-  selectedLanguage: any;
-  setSelectedLanguage: any;
-  languages: any;
+  selectedLanguage: string;
+  setSelectedLanguage: (language: string) => void;
+  languages: string[];
 }
 
-const LanguageSelector = (selectedLanguage, setSelectedLanguage, languages) => (
+const LanguageSelector = ({selectedLanguage, setSelectedLanguage, languages}: LanguageSelectorProps) => (
   <div className="cursor-pointer rounded-full space-x-1 pl-2 bg-black flex items-center flex-row">
     <Languages size={22} />
     <select
@@ -15,7 +15,7 @@ const LanguageSelector = (selectedLanguage, setSelectedLanguage, languages) => (
       onChange={(e) => setSelectedLanguage(e.target.value)}
       className="flex flex-row bg-black py-1 text-white"
     >
-      {languages.map((language) => (
+      {languages.map((language: string) => (
         <option key={language} value={language}>
           {language}
         </option>
